e027a03efe325aa8ffe144b5d838d091c1e38219
JonathanSanchezM <jonysanchez382@gmail.com>
2020-11-26 11:36:31 -0600

    String concatenacion

---
+++ src/EjemploStringConcatenacion.java
@@ -0,0 +1,16 @@
+public class EjemploStringConcatenacion {
+    public static void main(String[] args) {
+        String curso = "Programacion Java";
+        String profesor = "Andres Guzman ";
+
+        String detalle = curso + " con el instructor " + profesor;
+        System.out.println(detalle);
+
+        int numA = 10;
+        int numB = 5;
+
+        System.out.println(detalle + (numA + numB));
+
+        System.out.println(numA + numB + detalle);
+    }
+}
